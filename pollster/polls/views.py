aa709e2a8666f2d0afcfb57c62771102e193f276
rafasfz <rafaelsilvafreire2@gmail.com>
2021-09-21 21:43:07 -0300

    Views e url para detalhes e resultados

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question, Choice
 
@@ -7,3 +7,15 @@ def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
   context = {'latest_question_list': latest_question_list}
   return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, 'polls/detail.html', { 'question': question })
+
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', { 'question': question })
